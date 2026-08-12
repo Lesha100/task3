62b045ede3155527d53aad1979ac5dc1c99b7ad7
Lesha <leshaokotchik@gmail.com>
2026-08-12 17:43:18 +0300

    qwe

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$x = $_GET["x"] ?? null;
+$y = $_GET["y"] ?? null;
+
+if (
+    !preg_match('/^[1-9]\d*$/', $x ?? '') || !preg_match('/^[1-9]\d*$/', $y ?? '')
+) {
+    echo 'NaN';
+    exit;
+}
+
+$a = $x;
+$b = $y;
+
+while ($b !== '0') {
+    $t = bcmod($a, $b);
+    $a = $b;
+    $b = $t;
+}
+
+echo bcmul(bcdiv($x, $a), $y);
